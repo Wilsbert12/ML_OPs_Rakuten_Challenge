0379f06e1af773b96beb1be2f1929aedf4f605e0
Peter Stieg <PeterStieg@gmx.de>
2025-07-11 14:20:36 +0200

    feat: add page for flowcharts

---
+++ pages/4_Flowcharts.py
@@ -0,0 +1,18 @@
+# 4_Flowcharts.py
+import streamlit as st
+import mermaid as md
+from containers.rakuten_st.streamlit_utils import add_pagination_and_footer
+
+st.set_page_config(
+    page_title="MAY25 BDS // Flowcharts",
+    page_icon="images/logos/rakuten-favicon.ico",
+    layout="wide",
+)
+
+st.progress(4 / 7)
+st.title("Flowcharts")
+
+
+# Pagination and footer
+st.markdown("---")
+add_pagination_and_footer("pages/4_Flowcharts.py")

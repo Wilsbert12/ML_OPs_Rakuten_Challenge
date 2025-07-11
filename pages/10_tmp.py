9e73df3ae6c2b2e14cc570d2a23fd5b480676dc3
Peter Stieg <PeterStieg@gmx.de>
2025-07-11 14:21:09 +0200

    feat: add temporary page as sandbox

---
+++ pages/10_tmp.py
@@ -0,0 +1,8 @@
+# pages/10_tmp.py
+import streamlit as st
+import pandas as pd
+from containers.rakuten_st.streamlit_utils import add_pagination_and_footer
+
+# Pagination and footer
+st.markdown("---")
+add_pagination_and_footer("pages/10_tmp.py")
